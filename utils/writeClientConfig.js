195f5e218f24eaa7fe05f6ae47f3ffb6ecfb3741
Abramstyle <abram.style@gmail.com>
2018-03-26 16:45:32 +0800

    fix: fix incorrect app existent check

--- utils/writeClientConfig.js
+++ utils/writeClientConfig.js
@@ -11,7 +11,7 @@ const template = (config) => {
       const { default: routes } = require('${routes}');
       const { default: configureStore } = require('${store}');
       const stats = require('${build.target}/react-loadable.json');
-      const App = app ? require('${app}') : null;
+      const App = ${!!app} ? require('${app}') : null;
 
       exports.routes = routes;
       exports.configureStore = configureStore;
