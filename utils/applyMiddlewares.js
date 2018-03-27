6093de7e236aaf6bcbc5727b3e9732b3e9325722
Abramstyle <abram.style@gmail.com>
2018-03-27 22:09:32 +0800

    fix: show error detail.

--- utils/applyMiddlewares.js
+++ utils/applyMiddlewares.js
@@ -1,11 +1,12 @@
 async function loadMiddlewares(middlewarePath, app) {
+  let applyMiddlewares = () => {};
   try {
-    const applyMiddlewares = require(middlewarePath);
-    if (typeof applyMiddlewares === 'function') {
-      applyMiddlewares(app);
-    }
+    applyMiddlewares = require(middlewarePath);
   } catch (e) {
     console.warn('middleware path specified, but load middleware failed.');
+  }
+  if (typeof applyMiddlewares === 'function') {
+    applyMiddlewares(app);
   }
 }
 
