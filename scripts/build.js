a9213fc10a4ba3c737a328ba49179ae4a10b1f1b
Abramstyle <abram.style@gmail.com>
2018-03-28 17:22:15 +0800

    refactor: refactor builder structure

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,7 @@
 const loadConfig = require('../utils/loadConfig');
-const loadBuildConfig = require('../utils/loadBuildConfig');
-const Compiler = require('../lib/compiler');
-const cleanBuildDir = require('../utils/cleanBuildDir');
+const builder = require('../lib/builder');
 
 async function build(options) {
-  await cleanBuildDir(options);
   // const { env: { NODE_ENV: env } } = process;
   const { config: configPath } = options;
   let config = null;
@@ -19,16 +16,7 @@ async function build(options) {
     throw new Error('config not found');
   }
 
-  const buildConfig = loadBuildConfig(config);
-
-  const compiler = new Compiler(buildConfig.client);
-
-  console.log('building resources...');
-  return compiler.run()
-    .then((stats) => {
-      console.log('build success.');
-      return Promise.resolve(stats.toJson());
-    });
+  builder(config);
 }
 
 module.exports = build;
