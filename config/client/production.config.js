477e5c44c7ee0cd4990ffcb6628d0fb304d426fc
Abramstyle <abram.style@gmail.com>
2018-04-09 10:00:58 +0800

    fix: webpack v4 configurations

--- config/client/production.config.js
+++ config/client/production.config.js
@@ -129,10 +129,7 @@ const getConfig = config => ({
     namedModules: true,
     splitChunks: {
       name: 'commons',
-      minChunks(module) {
-        // this assumes your vendor imports exist in the node_modules directory
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      },
+      minChunks: 2,
     },
   },
 });
